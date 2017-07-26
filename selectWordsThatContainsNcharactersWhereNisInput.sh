07d135c31122b16bc6b6375b4a5c6a4522c8ce27
belal <belalmostafa30@gmail.com>
2017-07-26 21:45:14 +0200

    handling user inputs

--- selectWordsThatContainsNcharactersWhereNisInput.sh
+++ selectWordsThatContainsNcharactersWhereNisInput.sh
@@ -2,8 +2,7 @@
 fileNameThatContainsTheWords=~/wordList.txt
 wordsCountInTheFile=$(grep -Ec "^[a-zA-Z]{$1}$" $fileNameThatContainsTheWords)
 echo "words count " $wordsCountInTheFile
-myWords=$(grep -E "^[a-zA-Z]{5}$" $fileNameThatContainsTheWords > ./temp.txt)
-echo "words are " $myWords
+myWords=$(grep -E "^[a-zA-Z]{$1}$" $fileNameThatContainsTheWords > ./temp.txt)
 randomWord=$(($RANDOM%$wordsCountInTheFile+1))
 echo "index of the word to be printed " $randomWord
 word=$(sed -n "${randomWord}p" ./temp.txt)
---
+++ userInput.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Username: " userName
+read -sp "Password: " pass
+echo 
+echo welcome $userName I am really happy to meet you
+
+echo can you tell us what the top 3 programming language you advice 'for' others
+read prog1 prog2 prog3
+
+echo your 1st is: $prog1
+echo your 2nd is: $prog2
+echo your 3rd is: $prog3
